3bada9c5065cc9f1d5a3e5a277ece63bb2e03730
ispan <ispan@DESKTOP-PAKSETB>
2022-10-28 19:05:53 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.InputTextBox = new System.Windows.Forms.TextBox();
+			this.CalcButton = new System.Windows.Forms.Button();
+			this.ResultTextBox = new System.Windows.Forms.TextBox();
+			this.NewGameButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// InputTextBox
+			// 
+			this.InputTextBox.Location = new System.Drawing.Point(91, 44);
+			this.InputTextBox.Name = "InputTextBox";
+			this.InputTextBox.Size = new System.Drawing.Size(157, 22);
+			this.InputTextBox.TabIndex = 0;
+			// 
+			// CalcButton
+			// 
+			this.CalcButton.Location = new System.Drawing.Point(304, 42);
+			this.CalcButton.Name = "CalcButton";
+			this.CalcButton.Size = new System.Drawing.Size(75, 23);
+			this.CalcButton.TabIndex = 1;
+			this.CalcButton.Text = "button1";
+			this.CalcButton.UseVisualStyleBackColor = true;
+			this.CalcButton.Click += new System.EventHandler(this.CalcButton_Click);
+			// 
+			// ResultTextBox
+			// 
+			this.ResultTextBox.Location = new System.Drawing.Point(91, 126);
+			this.ResultTextBox.Multiline = true;
+			this.ResultTextBox.Name = "ResultTextBox";
+			this.ResultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.ResultTextBox.Size = new System.Drawing.Size(313, 197);
+			this.ResultTextBox.TabIndex = 2;
+			// 
+			// NewGameButton
+			// 
+			this.NewGameButton.Location = new System.Drawing.Point(304, 81);
+			this.NewGameButton.Name = "NewGameButton";
+			this.NewGameButton.Size = new System.Drawing.Size(75, 23);
+			this.NewGameButton.TabIndex = 3;
+			this.NewGameButton.Text = "button1";
+			this.NewGameButton.UseVisualStyleBackColor = true;
+			this.NewGameButton.Click += new System.EventHandler(this.NewGameButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(504, 360);
+			this.Controls.Add(this.NewGameButton);
+			this.Controls.Add(this.ResultTextBox);
+			this.Controls.Add(this.CalcButton);
+			this.Controls.Add(this.InputTextBox);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox InputTextBox;
+		private System.Windows.Forms.Button CalcButton;
+		private System.Windows.Forms.TextBox ResultTextBox;
+		private System.Windows.Forms.Button NewGameButton;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		private GuessNumber game;
+		public Form1()
+		{
+			InitializeComponent();
+			game = new GuessNumber();
+			game.NewGame();
+		}
+
+		private void CalcButton_Click(object sender, EventArgs e)
+		{
+			string input = InputTextBox.Text;
+			int[] inputArray = new int[4];
+			var game = new GuessNumber();
+			for (int i = 0; i < 4; i++)
+			{
+				inputArray[i] =Convert.ToInt32( input.Substring(i, 1));
+			}
+			string oldHint = ResultTextBox.Text;
+			string Hint =$"{oldHint} \r\n{game.Guess(inputArray)}";
+			ResultTextBox.Text = Hint;
+
+		}
+
+		private void NewGameButton_Click(object sender, EventArgs e)
+		{
+			game.NewGame();
+		}
+	}
+
+	public class GuessNumber
+	{
+		private int[] answer { get ; set; }
+		public void NewGame()
+		{
+			int seed = Guid.NewGuid().GetHashCode();
+			int[] answerb = new int[4];
+			for (int i = 0; i < 4; i++)
+			{
+				var random = new Random(seed);
+				answerb[i] = random.Next(0, 10);
+
+				for (int k = 0; k < i; k++)
+				{
+					while (answerb[i] == answer[k])
+					{
+						k = 0;
+						answerb[i] = random.Next(0, 10);
+					}
+				}
+			}
+			answer = answerb;
+			
+		}
+		public string Guess(int[]inputArray)
+		{
+			int aNumber = 0;
+			int bNumber = 0;
+			 for (int i = 0; i < 4; i++)
+			{
+				for (int k = 0; k < 4; k++)
+				{
+					if (inputArray[i] == this.answer[k]&&i==k)
+						aNumber++;
+					else if(inputArray[i] == this.answer[k]&&i!=k)
+						bNumber++;
+				}
+			}
+			string result = $"{aNumber}A{bNumber}B {answer[0]}{answer[1]}{answer[2]}{answer[3]}";
+			return result;
+		}
+	}
+}
